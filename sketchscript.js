04030f91e854c6819fe7acfe1936c294779fed1d
unknown <rwgillespieofcambridge@gmail.com>
2021-06-22 20:58:47 -0400

    I can select any square to make it red

--- sketchscript.js
+++ sketchscript.js
@@ -15,12 +15,11 @@ let item = null;
 //fill the grid with little squares to make a perfect square
 for (i=0; i < (yourNum * yourNum); i++) {
     item = document.createElement('div' + i);
-    console.log(item);
     item.style.border="solid";
     item.style.width="50px";
     item.style.height="50px";
-    item.addEventListener('mouseover', function() {
-        item.style.backgroundColor="red";
-    })
     container.appendChild(item);
 }
+container.addEventListener('mouseover', function(e){
+    e.target.style.backgroundColor="red";
+})
